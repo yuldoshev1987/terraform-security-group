373f789fb1eb0a4dba466d2ef0797173489b2df7
Owner <yuldoshev1987@gmail.com>
2020-08-24 08:43:24 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+variable "ingress_ports" {
+  type = list(number)
+  default = [80,8080,443]
+}
+variable "ssh_publicip" {
+  type = string
+  default = "173.48.205.16/32"
+}
